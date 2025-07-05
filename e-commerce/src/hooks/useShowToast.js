d4cf4b7c0868633607f26c74bc3e9895d6b77421
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-05 21:32:35 +0600

    create toast

---
+++ e-commerce/src/hooks/useShowToast.js
@@ -0,0 +1,22 @@
+import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
+
+const useShowToast = () => {
+  const toast = useToast();
+
+  const showToast = useCallback(
+    (title, des, status) => {
+      toast({
+        title: title,
+        description: des,
+        status: status,
+        duration: 3000,
+        isClosable: true,
+      });
+    },
+    [toast]
+  );
+  return showToast;
+};
+
+export default useShowToast;
