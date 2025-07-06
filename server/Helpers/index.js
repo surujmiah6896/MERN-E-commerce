c4738a08c9d412997dd887c88b98ecdec4294ee9
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-06 13:15:19 +0600

    create auth check middelware route

--- server/Helpers/index.js
+++ server/Helpers/index.js
@@ -1,11 +1,20 @@
 const helper = {};
 
-helper.sendWithError = (res, code, status, message) =>{
+helper.sendWithError = (res, code = 500, status=false, message) =>{
     const resp = res.status(code).json({
       status,
       message
     });
     return resp;
 }
+
+helper.sendWithData = (res, code = 200, status = true, data=null, message) =>{
+  const resp = res.status(code).json({
+    status,
+    data,
+    message,
+  });
+  return resp;
+};
 
 module.exports = helper;
--- server/routes/auth/authRoutes.js
+++ server/routes/auth/authRoutes.js
@@ -3,13 +3,19 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  authCheckMiddleware,
 } = require("../../controllers/auth/authController");
+const { sendWithData } = require('../../Helpers');
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/check-auth", authCheckMiddleware, (req, res)=>{
+  const user = req.user;
+  sendWithData(res, 200, true, user, "Authenticated User!");
+});
 
 
 module.exports = router;
