ab833f3ac67186c4d3a1c703fd1e5df7809c070e
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-06 18:58:14 +0600

    create admin header and sidebar

--- e-commerce/src/components/admin/layout.jsx
+++ e-commerce/src/components/admin/layout.jsx
@@ -1,15 +1,17 @@
 import { Box, Flex } from '@chakra-ui/react'
 import { Outlet } from "react-router-dom";
+import AdminSideBar from './sidebar';
+import AdminHeader from './header';
 
 const AdminLayout = () => {
   return (
     <Flex minH="100vh" w="full">
         {/* admin sidebar */}
-            admin side bar
+            <AdminSideBar/>
         {/* main content */}
         <Flex flex={1} direction={"column"}>
             {/* admin header */}
-            admin header
+            <AdminHeader/>
 
             {/* main area */}
             <Box flex={1} bg="gray.50" p={{base:4, md:6}}>
---
+++ e-commerce/src/components/admin/sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminSideBar = () => {
+  return (
+    <div>
+      AdminSideBar
+    </div>
+  )
+}
+
+export default AdminSideBar;
