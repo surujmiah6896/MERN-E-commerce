860442a4a1d188beaa1e7a5ac726674a069ef07e
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-06 19:55:46 +0600

    admin header design

--- e-commerce/src/components/admin/header.jsx
+++ e-commerce/src/components/admin/header.jsx
@@ -1,12 +1,47 @@
-import { Text } from '@chakra-ui/react';
-import React from 'react'
+import {
+  Box,
+  Flex,
+  IconButton,
+  Button,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { AlignJustify, LogOut } from "lucide-react";
 
-const AdminHeader = () => {
+const AdminHeader = ({ setOpen, handleLogout }) => {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
+
   return (
-    <div>
-       <Text color={'black'}>Admin header</Text>
-    </div>
-  )
-}
+    <Box
+      as="header"
+      px={4}
+      py={3}
+      borderBottom="1px"
+      borderColor="gray.200"
+      bg="gray.50"
+    >
+      <Flex align="center" justify="space-between">
+        {isMobile && (
+          <IconButton
+            icon={<AlignJustify />}
+            aria-label="Toggle Menu"
+            onClick={() => setOpen(true)}
+            display={{ base: "block", lg: "none" }}
+          />
+        )}
+
+        <Flex flex="1" justify="flex-end">
+          <Button
+            onClick={handleLogout}
+            leftIcon={<LogOut />}
+            colorScheme="blue"
+            variant="solid"
+          >
+            Logout
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
 
 export default AdminHeader;
