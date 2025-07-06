54f6f1ebdea6ec9d0caa10579322c690dea416bc
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-06 12:50:15 +0600

    create logout slice

--- e-commerce/src/store/auth-slice/index.js
+++ e-commerce/src/store/auth-slice/index.js
@@ -1,5 +1,5 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import { userStore, userLogin } from "../api/authApi";
+import { userStore, userLogin, userLogout } from "../api/authApi";
 
 const initialState = {
     isAuthenticated: false,
@@ -44,6 +44,24 @@ export const loginUser = createAsyncThunk(
 )
 
 
+//user logout
+
+export const logoutUser = createAsyncThunk(
+    "auth/logout",
+    async ({rejectWithValue}) => {
+        try{
+            const response = await userLogout();
+            return response.data;
+        }catch(err){
+            if(err.response && err.response.data){
+                return rejectWithValue(err.response.data);
+            }else{
+                return rejectWithValue({ message: err.message });
+            }
+        }
+    }
+)
+
 
 
 const authSlice = createSlice({
@@ -53,37 +71,47 @@ const authSlice = createSlice({
         setUser: (state, action) => {}
     },
     extraReducers: (builder) => {
-        builder.addCase(registerUser.pending, (state) =>{
+        builder
+          .addCase(registerUser.pending, (state) => {
             state.isError = false;
             state.isLoading = true;
-        })
-        .addCase(registerUser.fulfilled, (state) => {
+          })
+          .addCase(registerUser.fulfilled, (state) => {
             state.isLoading = false;
             state.isError = false;
             state.user = null;
             state.isAuthenticated = false;
-        })
-        .addCase(registerUser.rejected, (state, action) =>{
+          })
+          .addCase(registerUser.rejected, (state, action) => {
             state.isError = true;
             state.error = action.error?.message;
             state.isLoading = false;
             state.user = null;
             state.isAuthenticated = false;
-        }).addCase(loginUser.pending, (state) =>{
-            state.isError=false;
-            state.isLoading= true;
-        }).addCase(loginUser.fulfilled, (state, action)=>{
+          })
+          .addCase(loginUser.pending, (state) => {
+            state.isError = false;
+            state.isLoading = true;
+          })
+          .addCase(loginUser.fulfilled, (state, action) => {
             state.isLoading = false;
             state.isError = false;
             state.user = action?.payload?.status ? action.payload.user : null;
             state.isAuthenticated = action.payload.status;
-        }).addCase(loginUser.rejected, (state, action) => {
+          })
+          .addCase(loginUser.rejected, (state, action) => {
             state.isError = true;
             state.error = action.error?.message;
             state.isLoading = false;
             state.user = null;
             state.isAuthenticated = false;
-        });
+          })
+          .addCase(logoutUser.fulfilled, (state)=>{
+            state.isLoading = false,
+            state.isError = false,
+            state.user = null,
+            state.isAuthenticated = false
+          });
     }
 });
 export const { setUser } = authSlice.actions;
