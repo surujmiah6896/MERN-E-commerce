a4c7849c7a58096b5766d22ba2c9d3b1809da8a7
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-08 23:35:11 +0600

    shopping cart add thunk

--- e-commerce/src/store/cart-slice/index.js
+++ e-commerce/src/store/cart-slice/index.js
@@ -1,9 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   cartItems: [],
   isLoading: false,
 };
+
+export const addToCart = createAsyncThunk(
+  "cart/addToCart",
+  async ({ userId, productId, quantity }) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/shop/cart/add",
+      {
+        userId,
+        productId,
+        quantity,
+      }
+    );
+
+    return response.data;
+  }
+);
 
 const shoppingCartSlice = createSlice({
     name:"shoppingCart",
