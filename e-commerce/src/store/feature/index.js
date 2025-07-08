0ba185aafb7355e021549597072e15bdcaf08eb5
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-08 19:17:29 +0600

    Feature slice create thunk

--- e-commerce/src/store/feature/index.js
+++ e-commerce/src/store/feature/index.js
@@ -1,9 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isLoading: false,
   featureImageList: [],
 };
+
+export const addFeatureImage = createAsyncThunk(
+  "/order/addFeatureImage",
+  async (image) => {
+    const response = await axios.post(
+      `http://localhost:5000/api/feature/add`,
+      { image }
+    );
+    return response.data;
+  }
+);
 
 const featureSlice = createSlice({
     name:"featureSlice",
