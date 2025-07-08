20cb5168003fcb1121562dd51c1156efdf301d66
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-08 19:25:08 +0600

    Feature get slice create

--- e-commerce/src/store/feature/index.js
+++ e-commerce/src/store/feature/index.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const initialState = {
   isLoading: false,
-  featureImageList: [],
+  error: "",
+  featureImages: [],
 };
 
 export const addFeatureImage = createAsyncThunk(
@@ -17,12 +18,50 @@ export const addFeatureImage = createAsyncThunk(
   }
 );
 
+export const getFeatureImages = createAsyncThunk(
+  "/order/getFeatureImages",
+  async () => {
+    const response = await axios.get(
+      `http://localhost:5000/api/feature/get`
+    );
+
+    return response.data;
+  }
+);
+
 const featureSlice = createSlice({
     name:"featureSlice",
     initialState,
     reducers:{},
     extraReducers: (builder)=>{
-
+        builder
+          .addCase(addFeatureImage.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(addFeatureImage.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.featureImages = action?.payload?.status
+              ? action?.payload?.data
+              : null;
+          })
+          .addCase(addFeatureImage.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action?.payload?.error;
+          })
+          .addCase(getFeatureImages.pending, (state) => {
+            state.isLoading = true;
+          })
+          .addCase(getFeatureImages.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.featureImages = action?.payload?.status
+              ? action?.payload?.data
+              : null;
+          })
+          .addCase(getFeatureImages.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action?.payload?.error;
+            state.featureImages = [];
+          });;
     }
 });
 
