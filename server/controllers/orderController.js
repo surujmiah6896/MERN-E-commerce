b9922afbeb9ad0abcbbf943c1053eb1f3fb02526
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-09 20:40:56 +0600

    create order

---
+++ server/controllers/orderController.js
@@ -0,0 +1,61 @@
+
+const { sendWithData, sendWithResponse } = require("../Helpers");
+const Cart = require("../models/Cart");
+const Order = require("../models/Order");
+const Product = require("../models/Product");
+
+const orderController = {};
+
+orderController.createOrder = async (req, res) => {
+  try {
+    const {
+      userId,
+      cartItems,
+      addressInfo,
+      orderStatus,
+      paymentMethod,
+      paymentStatus,
+      totalAmount,
+      orderDate,
+      orderUpdateDate,
+      paymentId,
+      payerId,
+      cartId,
+    } = req.body;
+
+        const newlyCreatedOrder = new Order({
+          userId,
+          cartId,
+          cartItems,
+          addressInfo,
+          orderStatus,
+          paymentMethod,
+          paymentStatus,
+          totalAmount,
+          orderDate,
+          orderUpdateDate,
+          paymentId,
+          payerId,
+        });
+
+        await newlyCreatedOrder.save();
+
+        const sendData = {
+          orderId: newlyCreatedOrder._id,
+        };
+
+        return sendWithData(res, 201, true, sendData, "order Create Successfully");
+  } catch (e) {
+    console.log(e);
+    return sendWithResponse(res, 500, false, "Some order create error");
+  }
+};
+
+
+
+module.exports = {
+  createOrder,
+  capturePayment,
+  getAllOrdersByUser,
+  getOrderDetails,
+};
