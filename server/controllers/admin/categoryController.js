690a340f31cdf6ef9c781df80af6ceb822e09781
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-12 13:42:24 +0600

    category update

--- server/controllers/admin/categoryController.js
+++ server/controllers/admin/categoryController.js
@@ -39,5 +39,33 @@ categoryController.getAllCategory = async (req, res) =>{
     }
 }
 
+categoryController.updateCategory = async(req, res) =>{
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        if (!name) {
+            return sendWithResponse(res, 400, false, "Name is required");
+        }
+
+        const category = await Category.findById(id);
+        if (!category) {
+            return sendWithResponse(res, 404, false, "Category not found");
+        }
+
+        const existingCategory = await Category.findOne({ name, _id: { $ne: id } });
+        if (existingCategory) {
+            return sendWithResponse(res, 409, false, "Category name already exists");
+        }
+
+        category.name = name;
+        const updatedCategory = await category.save();
+
+        return sendWithData(res, 200, true, updatedCategory, "Category updated successfully!");
+    } catch (error) {
+        return sendWithResponse(res, 500, false, "Failed to update category");
+    }
+}
+
 
 module.exports = categoryController;
--- server/routes/admin/category-routes.js
+++ server/routes/admin/category-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   storeCategory,
   getAllCategory,
+  updateCategory,
 } = require("../../controllers/admin/categoryController");
 
 const router = express.Router();
 
 router.post("/add", storeCategory);
 router.get('/get', getAllCategory);
+router.put('/update?id', updateCategory);
 
 module.exports = router;
